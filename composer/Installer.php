df679043b14bed64e53df49cf80680597b7f3114
faissaloux <fwahabali@gmail.com>
2025-09-10 14:36:24 +0100

    get latest release binaries

--- composer/Installer.php
+++ composer/Installer.php
@@ -8,6 +8,8 @@ use Composer\Plugin\PluginInterface;
 
 class Installer implements PluginInterface
 {
+    private string $repository = "faissaloux/pdf-suite";
+
     public function activate(Composer $composer, IOInterface $io)
     {
         $this->install();
@@ -37,15 +39,51 @@ class Installer implements PluginInterface
             throw new \Exception("Unsupported OS: $os");
         }
 
-        $binary = $binaries[$os];
-        $url = "https://github.com/faissaloux/pdf-suite/releases/latest/download/$binary";
+        $latestReleaseBinaries = $this->getLatestReleaseBinaries();
+        $binary = $latestReleaseBinaries[$binaries[$os]];
 
         $target = dirname(__DIR__, 3) . '/bin/pdf_suite' . ($os === 'Windows' ? '.exe' : '');
 
-        file_put_contents($target, fopen($url, 'r'));
+        file_put_contents($target, fopen($binary, 'r'));
 
         if ($os !== 'Windows') {
             chmod($target, 0755);
         }
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    private function getLatestReleaseBinaries(): array
+    {
+        $url = "https://api.github.com/repos/{$this->repository}/releases";
+
+        $opts = [
+            "http" => [
+                "header" => "User-Agent: pdf-suite\r\n"
+            ]
+        ];
+        $context = stream_context_create($opts);
+        $json = file_get_contents($url, false, $context);
+        $releases = json_decode($json, true);
+
+        $latestRelease = null;
+        foreach ($releases as $release) {
+            if ($release) {
+                $latestRelease = $release;
+                break; // Releases are sorted newest → oldest.
+            }
+        }
+
+        if (!$latestRelease) {
+            die("No release found.\n");
+        }
+
+        $assets = [];
+        foreach ($latestRelease["assets"] as $asset) {
+            $assets[$asset['name']] = $asset['browser_download_url'];
+        }
+
+        return $assets;
     }
 }
